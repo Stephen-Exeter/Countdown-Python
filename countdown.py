5e6b4a96aee371a5118dcb0761906d4dc07a0e92
Stephen-Exeter <st1018@exeter.ac.uk>
2025-09-30 17:24:40 +0100

    select_characters start

--- countdown.py
+++ countdown.py
@@ -1,1 +1,22 @@
+import random
+import string
 
+def letter_select(character_choices,vowel_true):
+    while True:
+        vowels = ['a','e','i','o','u','A','E','I','O','U']
+        letter = random.choice(character_choices)
+        if letter in vowels == vowel_true:
+            return letter
+
+def select_characters():
+    end_str = ""
+    user_inp = input("type \'c\' for a consonant and \'v\' for a vowel")
+    if user_inp == "v":
+        end_str += letter_select(string.ascii_letters,True)
+    else:
+        end_str += letter_select(string.ascii_letters,False)
+    # for _ in range(7):
+    #     user_inp = input("type \'c\' for a consonant and \'v\' for a vowel")
+    return end_str
+        
+print(select_characters())
